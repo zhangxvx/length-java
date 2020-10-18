232b0d560e429ed8d7aa229774e27e0c56337c66
Vic Wu <madcocomo@gmail.com>
2020-10-18 16:04:33 +0800

    start task 3

--- src/main/java/xp/io/refactor/Length.java
+++ src/main/java/xp/io/refactor/Length.java
@@ -1,41 +1,45 @@
 package xp.io.refactor;
 
 public class Length {
-  private final double val;
-  private final String uinnt;
+    private final double val;
+    private final String uinnt;
 
-  public Length(double val, String uinnt) {
-    this.val = val;
-    this.uinnt = uinnt;
-  }
+    public Length(double val, String uinnt) {
+        this.val = val;
+        this.uinnt = uinnt;
+    }
 
-  public Length as(String u) {
-    Length len = this;
-    if (this.uinnt.equals("f") && u.equals("inch")) len = new Length(val * 12, u);
-    if (this.uinnt.equals("inch") && u.equals("f")) len = new Length(val / 12, u);
+    public Length as(String u) {
+        Length len = this;
+        if (this.uinnt.equals("f") && u.equals("inch")) {
+            len = new Length(val * 12, u);
+        }
+        if (this.uinnt.equals("inch") && u.equals("f")) {
+            len = new Length(val / 12, u);
+        }
 
-      if (this.uinnt.equals("yard")) {
-          if (u.equals("inch")) {
-              len = new Length(val * 36, u);
-          } else if (u.equals("f")){
-              len = new Length(val * 3, u);
-          }
-      } else if (u.equals("yard")) {
-          if (this.uinnt.equals("f")) {
-              len = new Length(val / 3, u);
-          } else if (this.uinnt.equals("inch")) {
-              len = new Length(val / 36, u);
-          }
-      }
+        if (this.uinnt.equals("yard")) {
+            if (u.equals("inch")) {
+                len = new Length(val * 36, u);
+            } else if (u.equals("f")){
+                len = new Length(val * 3, u);
+            }
+        } else if (u.equals("yard")) {
+            if (this.uinnt.equals("f")) {
+                len = new Length(val / 3, u);
+            } else if (this.uinnt.equals("inch")) {
+                len = new Length(val / 36, u);
+            }
+        }
 
-    return len;
-  }
+        return len;
+    }
 
-  public double getVal() {
-    return this.val;
-  }
+    public double getVal() {
+        return this.val;
+    }
 
-  public String getUnit() {
-    return this.uinnt;
-  }
+    public String getUnit() {
+        return this.uinnt;
+    }
 }
