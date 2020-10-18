ed74406f81c933bc8bb3580fc2fcb12342234cc4
Vic Wu <madcocomo@gmail.com>
2020-10-18 15:48:43 +0800

    add naming issue

--- src/main/java/xp/io/refactor/Length.java
+++ src/main/java/xp/io/refactor/Length.java
@@ -1,30 +1,30 @@
 package xp.io.refactor;
 
 public class Length {
-  private final double value;
-  private final String u;
+  private final double val;
+  private final String uinnt;
 
-  public Length(double val, String u) {
-    this.value = val;
-    this.u = u;
+  public Length(double val, String uinnt) {
+    this.val = val;
+    this.uinnt = uinnt;
   }
 
   public Length as(String u) {
     Length len = this;
-    if (this.u.equals("f") && u.equals("inch")) len = new Length(value * 12, u);
-    if (this.u.equals("inch") && u.equals("f")) len = new Length(value / 12, u);
+    if (this.uinnt.equals("f") && u.equals("inch")) len = new Length(val * 12, u);
+    if (this.uinnt.equals("inch") && u.equals("f")) len = new Length(val / 12, u);
 
-      if (this.u.equals("yard")) {
+      if (this.uinnt.equals("yard")) {
           if (u.equals("inch")) {
-              len = new Length(value * 36, u);
+              len = new Length(val * 36, u);
           } else if (u.equals("f")){
-              len = new Length(value * 3, u);
+              len = new Length(val * 3, u);
           }
       } else if (u.equals("yard")) {
-          if (this.u.equals("f")) {
-              len = new Length(value / 3, u);
-          } else if (this.u.equals("inch")) {
-              len = new Length(value / 36, u);
+          if (this.uinnt.equals("f")) {
+              len = new Length(val / 3, u);
+          } else if (this.uinnt.equals("inch")) {
+              len = new Length(val / 36, u);
           }
       }
 
@@ -32,10 +32,10 @@ public class Length {
   }
 
   public double getVal() {
-    return this.value;
+    return this.val;
   }
 
   public String getUnit() {
-    return this.u;
+    return this.uinnt;
   }
 }
